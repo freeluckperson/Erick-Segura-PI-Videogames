2d829bea948da9fdef2d3da90693f9f81830496d
freeluckperson <gestionecesaria@gmail.com>
2023-07-04 19:39:20 -0400

    primer commit

---
+++ client/src/componets/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Home.module.css'
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Soy el componente Home</h2>
+      <Link to='/' className={styles.navLink}>
+        <p>BACK</p>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/componets/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import styles from './LandingPage.css';
+import styles from './LandingPage.module.css';
+import { Link } from 'react-router-dom';
+
+const st = {fontSize: '3em'};
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Link to='/home' className={styles.navLink}>
+        <p style={st} >To Home</p>
+      </Link>
+    </div>
+  );
+};
+
+export default LandingPage;
